08b7a2c9d2e38158724ef220c5feb3737af7e8e4
Tobias Bodewig <tbodewig@web.de>
2014-08-23 16:55:24 +0200

    Double You

---
+++ Ludum-Dare-30/src/game/level/EntityPlayerClone.java
@@ -0,0 +1,45 @@
+package game.level;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class EntityPlayerClone extends EntityPlayer {
+
+	private Byte[] recording;
+	private int frame = 0;
+	private boolean isDead = false;
+
+	public EntityPlayerClone(double x, double y, Byte[] recording) {
+		super(x, y);
+		animationRun = new Animation();
+		animationJump = new Animation();
+		animationRun.load("/model/clone/Run-Animation.png", 2, 150);
+		animationJump.load("/model/clone/Jump-Animation.png", 2, 150);
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/model/clone/Player-Model.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.recording = recording;
+	}
+
+	public void update(LevelMap map) {
+		if (!isDead) {
+			byte movement = recording[frame];
+			key_W = movement > 2;
+			key_A = (movement % 3) == 2;
+			key_D = (movement % 3) == 1;
+			super.update(map);
+			frame++;
+			if (frame >= recording.length) {
+				isDead = true;
+			}
+		}
+	}
+
+	public boolean isDead() {
+		return isDead;
+	}
+
+}
