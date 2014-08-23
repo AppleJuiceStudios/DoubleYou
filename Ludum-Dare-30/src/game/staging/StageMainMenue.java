d29d17c181dedb7ff0a6ee3b2a882388981e0f0a
Philipp Auch <philipp.auch@web.de>
2014-08-23 10:06:26 +0200

    implementing Main Menue

--- Ludum-Dare-30/src/game/staging/StageMainMenue.java
+++ Ludum-Dare-30/src/game/staging/StageMainMenue.java
@@ -2,18 +2,84 @@ package game.staging;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Map;
 
 public class StageMainMenue extends Stage {
 
+	private Rectangle btnPlay;
+	private Rectangle btnOptions;
+	private Rectangle btnCredits;
+	private Rectangle btnLevelEditor;
+	private Rectangle btnExit;
+
 	public StageMainMenue(StageManager stageManager, Map<String, String> data) {
 		super(stageManager, data);
+		initMouse();
+		initRecs();
+	}
+
+	private void initMouse() {
+		getStageManager().setMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				Point point = new Point(x, y);
+
+				if (btnPlay.contains(point)) {
+					play();
+				} else if (btnOptions.contains(point)) {
+					options();
+				} else if (btnCredits.contains(point)) {
+					credits();
+				} else if (btnLevelEditor.contains(point)) {
+					levelEditor();
+				} else if (btnExit.contains(point)) {
+					exit();
+				}
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+	}
+
+	private void initRecs() {
+		btnPlay = new Rectangle(30, 300, 200, 40);
+		btnOptions = new Rectangle(30, 350, 200, 40);
+		btnCredits = new Rectangle(30, 400, 200, 40);
+		btnLevelEditor = new Rectangle(30, 450, 200, 40);
+		btnExit = new Rectangle(30, 500, 200, 40);
 	}
 
 	@Override
 	public void draw(Graphics2D g2) {
+		g2.setColor(Color.RED);
+		g2.fillRect(0, 0, 800, 600); //Background
 		g2.setColor(Color.GREEN);
-		g2.fillRect(10, 10, 50, 50);
+		g2.fill(btnPlay);
+		g2.fill(btnOptions);
+		g2.fill(btnCredits);
+		g2.fill(btnLevelEditor);
+		g2.fill(btnExit);
 	}
 
 	@Override
@@ -26,4 +92,23 @@ public class StageMainMenue extends Stage {
 
 	}
 
+	private void play() {
+		System.out.println("PLay");
+	}
+
+	private void options() {
+		System.out.println("Options");
+	}
+
+	private void credits() {
+		System.out.println("Credits");
+	}
+
+	private void levelEditor() {
+		System.out.println("LevelEditor");
+	}
+
+	private void exit() {
+		System.exit(0);
+	}
 }
