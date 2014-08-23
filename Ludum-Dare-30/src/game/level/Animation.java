a3c8a95ef787d8d8faed049312306f7326b73374
Philipp Auch <philipp.auch@web.de>
2014-08-23 14:14:26 +0200

    adding animations

---
+++ Ludum-Dare-30/src/game/level/Animation.java
@@ -0,0 +1,41 @@
+package game.level;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Animation {
+	public static final int WIDTH = 14;
+	public static final int HEIGHT = 31;
+
+	private int index = 0;
+	private int delay = 0;
+	private BufferedImage[] imgAnimation;
+
+	private long lastChange;
+
+	public Animation() {
+	}
+
+	public void load(String path, int imageAmount, int delay) {
+		this.delay = delay;
+		try {
+			BufferedImage image = ImageIO.read(getClass().getResourceAsStream(path));
+			imgAnimation = new BufferedImage[imageAmount];
+			for (int i = 0; i < imageAmount; i++) {
+				imgAnimation[i] = image.getSubimage(i * WIDTH, 0, WIDTH, HEIGHT);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public BufferedImage getImage(int animation) {
+		if (System.currentTimeMillis() > lastChange + delay) {
+			lastChange += delay;
+			index = (index + 1) % imgAnimation.length;
+		}
+		return imgAnimation[index];
+	}
+}
