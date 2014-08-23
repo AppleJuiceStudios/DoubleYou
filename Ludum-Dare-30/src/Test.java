c3068f3f8ff21b133cc4c4cb159ada563a84cb26
Tobias Bodewig <tbodewig@web.de>
2014-08-23 07:20:55 +0200

    Create Basic Frame/Applet.

--- Ludum-Dare-30/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("Ludum Dare 30");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("");
-	}
-}
