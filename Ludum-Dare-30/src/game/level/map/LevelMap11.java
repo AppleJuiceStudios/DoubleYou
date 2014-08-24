0ea753f4d69fbf4196261913371faea5e83406f4
Tobias Bodewig <tbodewig@web.de>
2014-08-24 15:10:09 +0200

    Edit TestLevel.

--- Ludum-Dare-30/src/game/level/map/LevelMap11.java
+++ Ludum-Dare-30/src/game/level/map/LevelMap11.java
@@ -7,11 +7,12 @@ import game.level.mapobject.MapObjectGroundswich;
 public class LevelMap11 extends LevelMap {
 
 	public void init() {
-		objects = new MapObject[4];
+		objects = new MapObject[5];
 		objects[0] = new MapObjectLasergate((byte) 32, 10, 12, 3, true);
 		objects[1] = new MapObjectGroundswich((byte) 33, 6, 14, (byte) 32, true);
-		objects[2] = new MapObjectLasergate((byte) 34, 21, 8, 5, true);
-		objects[3] = new MapObjectGroundswich((byte) 35, 17, 12, (byte) 34, false);
+		objects[2] = new MapObjectLasergate((byte) 34, 27, 8, 5, true);
+		objects[3] = new MapObjectGroundswich((byte) 35, 23, 12, (byte) 34, false);
+		objects[4] = new MapObjectGroundswich((byte) 36, 30, 12, (byte) 34, true);
 	}
 
 }
