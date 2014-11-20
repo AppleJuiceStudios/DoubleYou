280001ffb4a6f27faa8ba117a75d8d1180a34a23
PhilippAuch <philipp.auch@web.de>
2014-11-20 20:40:10 +0100

    remove unused imports

--- Ludum-Dare-30/src/game/res/SoundManager.java
+++ Ludum-Dare-30/src/game/res/SoundManager.java
@@ -1,15 +1,9 @@
 package game.res;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 import util.log.Log;
 
