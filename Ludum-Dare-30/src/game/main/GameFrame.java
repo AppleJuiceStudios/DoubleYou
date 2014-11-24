650fd75465195b2f255a2c18fd6a62299a19f4d2
PhilippAuch <philipp.auch@web.de>
2014-11-24 19:35:59 +0100

    adding icon to Frame

--- Ludum-Dare-30/src/game/main/GameFrame.java
+++ Ludum-Dare-30/src/game/main/GameFrame.java
@@ -1,22 +1,31 @@
 package game.main;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
+import java.net.URL;
 
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 	private static final long serialVersionUID = 4260463266395801740L;
-	
+
 	private GameCanvas gameCanvas;
 
 	public GameFrame() {
 		GameCanvas.IS_APPLET = false;
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Double You");
+
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Image img = kit.createImage(GameFrame.class.getResource("/dy_icon.png"));
+		setIconImage(img);
+
 		gameCanvas = new GameCanvas();
 		gameCanvas.init();
 		add(gameCanvas);
+
 		setResizable(false);
 		pack();
 		addWindowListener(new WindowAdapter() {
