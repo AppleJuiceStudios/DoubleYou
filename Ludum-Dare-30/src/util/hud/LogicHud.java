5bb34bae5cce8207cf5f5610088d4769a8d97817
PhilippAuch <philipp.auch@web.de>
2014-12-02 12:19:44 +0100

    First hud startings

---
+++ Ludum-Dare-30/src/util/hud/LogicHud.java
@@ -0,0 +1,15 @@
+package util.hud;
+
+import java.awt.Graphics2D;
+
+public class LogicHud extends Hud {
+
+	public LogicHud() {
+
+	}
+
+	@Override
+	public void draw(Graphics2D g2) {
+		super.draw(g2);
+	}
+}
