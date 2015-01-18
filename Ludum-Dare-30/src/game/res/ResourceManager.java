5c502d5359f06fb5c61bcaa8748f7f63469603c5
PhilippAuch <philipp.auch@web.de>
2015-01-18 16:28:23 +0100

    removing sysout

--- Ludum-Dare-30/src/game/res/ResourceManager.java
+++ Ludum-Dare-30/src/game/res/ResourceManager.java
@@ -66,7 +66,6 @@ public class ResourceManager {
 			Log.debug("Language-Name \"" + getString("language.name") + "\"");
 			Log.debug("Language-Region \"" + getString("language.region") + "\"");
 		} catch (NullPointerException e) {
-			System.out.println(local);
 			Log.error("Couldn't read lang file: \"" + local.toString() + "\"!");
 			e.printStackTrace();
 		} finally {
