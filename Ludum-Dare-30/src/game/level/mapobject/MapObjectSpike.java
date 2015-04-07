f04dfb8737658d920b83dedf63ea9c77d2046b65
PhilippAuch <philipp.auch@web.de>
2015-04-07 17:18:28 +0200

    adding xml to spike

--- Ludum-Dare-30/src/game/level/mapobject/MapObjectSpike.java
+++ Ludum-Dare-30/src/game/level/mapobject/MapObjectSpike.java
@@ -7,6 +7,9 @@ import game.res.ResourceManager;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class MapObjectSpike extends MapObject {
 
 	private BufferedImage image;
