7fb38fbe33dfbc8240bf608fb2fe2c334bd2acfb
PhilippAuch <philipp.auch@web.de>
2015-04-07 14:34:29 +0200

    adding MapObject Decoration

--- Ludum-Dare-30/src/game/level/mapobject/MapObjectDecoration.java
+++ Ludum-Dare-30/src/game/level/mapobject/MapObjectDecoration.java
@@ -1,28 +1,23 @@
 package game.level.mapobject;
 
-import game.res.ResourceManager;
-
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-public class MapObjectFinishFlag extends MapObject {
+public class MapObjectDecoration extends MapObject {
 
 	protected BufferedImage image;
 
-	public MapObjectFinishFlag(int id, int x, int y, int width, int height, boolean power) {
+	public MapObjectDecoration(int id, int x, int y, int width, int height, boolean power) {
 		super(id, x, y, 1, 2, false);
 		loadTexture();
 	}
 
-	public MapObjectFinishFlag() {
+	public MapObjectDecoration() {
 		loadTexture();
 	}
 
 	protected void loadTexture() {
-		image = ResourceManager.getImage("/model/finishFlag.png");
+
 	}
 
 	public boolean isSolid() {
@@ -32,5 +27,4 @@ public class MapObjectFinishFlag extends MapObject {
 	public void draw(Graphics2D g2, int size) {
 		g2.drawImage(image, x * size, y * size, size, size, null);
 	}
-
 }
