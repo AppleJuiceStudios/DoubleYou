857d96c2a1f50287bed282b99b917ab9f911bd59
PhilippAuch <philipp.auch@web.de>
2015-04-07 17:21:46 +0200

    adding spike to hud

--- Ludum-Dare-30/src/util/hud/ObjectHud.java
+++ Ludum-Dare-30/src/util/hud/ObjectHud.java
@@ -6,13 +6,14 @@ import game.level.mapobject.MapObjectLasergate;
 import game.level.mapobject.MapObjectLasergateClone;
 import game.level.mapobject.MapObjectLasergateHorizontal;
 import game.level.mapobject.MapObjectLasergateHorizontalClone;
+import game.level.mapobject.MapObjectSpike;
 import game.res.ResourceManager;
 
 public class ObjectHud extends Hud {
 
 	public ObjectHud() {
 		super(40, ORIENTATION_BOTTOM_CENTER);
-		items = new HudItem[6];
+		items = new HudItem[7];
 
 		items[0] = new HudItem(ResourceManager.getImage("/level/object/Groundswitch.png"), SIZE, MapObjectGroundswtich.class);
 		items[1] = new HudItem(ResourceManager.getImage("/level/object/Lasergate.png"), SIZE, MapObjectLasergate.class);
@@ -20,5 +21,6 @@ public class ObjectHud extends Hud {
 		items[3] = new HudItem(ResourceManager.getImage("/level/object/Lasergate-Horizontal.png"), SIZE, MapObjectLasergateHorizontal.class);
 		items[4] = new HudItem(ResourceManager.getImage("/level/object/Lasergate-HorizontalOnlyClone.png"), SIZE, MapObjectLasergateHorizontalClone.class);
 		items[5] = new HudItem(ResourceManager.getImage("/model/finishFlag.png"), SIZE, MapObjectDecorationFinishFlag.class);
+		items[6] = new HudItem(ResourceManager.getImage("/level/object/spike.png"), SIZE, MapObjectSpike.class);
 	}
 }
